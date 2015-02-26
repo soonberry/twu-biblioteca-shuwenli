5fcc5dd645fd20f6b4f4a69adfa021eecf6f0baa
soonberry <shuwenli89@126.com>
2015-02-27 01:47:58 +0800

    test for mockito

--- test/com/twu/biblioteca/BookTest.java
+++ test/com/twu/biblioteca/BookTest.java
@@ -10,9 +10,9 @@ public class BookTest {
     public void testDisplayBook() throws Exception {
         Book book = new Book("Stone", "Cao Xueqing", "Qing");
 
-        assertEquals("Stone", book.getName());
-        assertEquals("Cao Xueqing", book.getAuthor());
-        assertEquals("Qing", book.getYearPublished());
+//        assertEquals("Stone", book.getName());
+//        assertEquals("Cao Xueqing", book.getAuthor());
+//        assertEquals("Qing", book.getYearPublished());
 
         assertEquals("Stone,Cao Xueqing,Qing", book.toString());
 
---
+++ test/com/twu/biblioteca/MockitoTest.java
@@ -0,0 +1,38 @@
+package com.twu.biblioteca;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class MockitoTest {
+    @Test
+    public void testMockito() throws Exception {
+        List<String> list=mock(List.class);
+        //设置方法的预期返回值
+        when(list.get(0)).thenReturn("helloworld");
+
+        String result = list.get(0);
+
+        //验证方法调用(是否调用了get(0))
+        verify(list).get(0);
+
+        //junit测试
+        Assert.assertEquals("helloworld", result);
+
+
+        //arrange
+        Iterator i=mock(Iterator.class);
+        when(i.next()).thenReturn("Hello").thenReturn("World");
+        //act
+        String result1=i.next()+" "+i.next();
+        //verify
+        verify(i, times(2)).next();
+        //assert
+        assertEquals("Hello World", result1);
+    }
+}
